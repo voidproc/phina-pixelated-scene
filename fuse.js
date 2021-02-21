d913a8d746a7b71a03dfc9cbe6cd01fe851aaef5
voidProc <voidproc@gmail.com>
2021-02-22 01:36:52 +0900

    Update fuse-box 3 to 4

--- fuse.js
+++ fuse.js
@@ -1,51 +1,19 @@
-const { FuseBox, Sparky, UglifyESPlugin } = require('fuse-box');
+const path = require('path');
+const { fusebox } = require('fuse-box');
 
-const rootDir = 'dist/';
+const fuse = fusebox({
+  target: 'browser',
+  entry: 'src/index.js',
+  webIndex: {
+    template: 'src/index.html',
+  },
+  cache: true,
+  devServer: true,
+});
 
-Sparky.context({
-  getConfig() {
-    return FuseBox.init({
-      homeDir: 'src',
-      output: `${rootDir}js/$name.js`,
-      useTypescriptCompiler : true,
-      sourceMaps: !this.isProduction,
-      cache: !this.isProduction,
-      plugins: [
-        this.isProduction && UglifyESPlugin(),
-      ],
-    });
+fuse.runDev({
+  bundles: {
+    rootDir: 'dist',
+    app: 'js/app.js',
   }
-});
-
-Sparky.task('default', async context => {
-  const fuse = context.getConfig();
-  
-  fuse
-    .bundle('app')
-    .instructions('> index.js')
-    .watch()
-    .hmr({ reload: true });
-
-  fuse.dev({
-    port: 4444,
-    httpServer: true,
-    root: rootDir,
-  });
-  
-  return await fuse.run({
-    chokidar: {
-      usePolling: true,
-    }
-  });
-});
-
-Sparky.task('dist', async context => {
-  context.isProduction = true;
-  const fuse = context.getConfig();
-
-  fuse
-    .bundle('app')
-    .instructions('> index.js');
-
-  return await fuse.run()
 });
